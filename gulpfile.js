d4da47cf5fc15c130ad5f651eb0922b30e533ace
VohychSWS <volodymyr.pyshnyi@stairwaysoft.com>
2021-10-26 14:46:42 +0300

    finished version gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,94 +1,109 @@
-let prod = './prod/';
-let dev = './dev/';
+console.clear();
+const prod = './prod/';
+const dev = './dev/';
 
-let gulp = require('gulp');
-let sass = require('gulp-sass')(require('sass'));
-let babel = require('gulp-babel');
-let concat = require('gulp-concat');
-let uglify = require('gulp-uglify');
-let rename = require('gulp-rename');
-let cleanCSS = require('gulp-clean-css');
-let del = require('del');
-let autoprefixer = require('gulp-autoprefixer');
-let browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const cleanCSS = require('gulp-clean-css');
+const del = require('del');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+
+function browserHost() {
+    browserSync.init({
+        server: {
+            baseDir: prod,
+            open: true,
+            notify: false,
+            ui: false
+        },
+        tunnel: true,
+        host: 'localhost',
+        port: 8081,
+    })
+}
 
 const paths = {
     html: {
-      src: dev + 'index.html',
-      dest: prod
+        src: dev + 'index.html',
+        dest: prod
     },
     styles: {
-      src:  dev + 'styles/index.sass',
-      dest: prod + 'styles/'
+        src: dev + 'styles/index.sass',
+        dest: prod + 'styles/'
     },
     scripts: {
-      src: dev + 'scripts/*.js',
-      dest: prod + 'scripts/'
+        src: dev + 'scripts/*.js',
+        dest: prod + 'scripts/'
     }
 };
 
 function clean() {
-  return del([ 'prod' ]);
+    return del(['prod']);
 }
 
 function html() {
-  return gulp.src(paths.html.src)
-    .pipe(gulp.dest(paths.html.dest))
-    .pipe(browserSync.stream());
+    console.log('this')
+    return gulp.src(paths.html.src)
+        .pipe(gulp.dest(paths.html.dest))
+        .pipe(browserSync.reload({stream: true}))
+
 }
+
 function styles() {
-  return gulp.src(paths.styles.src)
-    .pipe(sass())
-    .pipe(cleanCSS())
-    .pipe(rename({
-      basename: 'main',
-      suffix: '.min'
-    }))
-    .pipe(autoprefixer({
-        overrideBrowserslist: ['last 5 versions'],
-    }))
-    .pipe(gulp.dest(paths.styles.dest))
-    .pipe(browserSync.stream());
+    return gulp.src(paths.styles.src)
+        .pipe(sass())
+        .pipe(cleanCSS())
+        .pipe(rename({
+            basename: 'main',
+            suffix: '.min'
+        }))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 5 versions'],
+        }))
+        .pipe(browserSync.reload({stream: true}))
+        .pipe(gulp.dest(paths.styles.dest))
 }
 
 function scripts() {
-  return gulp.src(paths.scripts.src, { sourcemaps: true })
-    .pipe(babel())
-    .pipe(uglify())
-    .pipe(concat('main.min.js'))
-    .pipe(gulp.dest(paths.scripts.dest))
-    .pipe(browserSync.stream());
+    return gulp.src(paths.scripts.src, {sourcemaps: true})
+        .pipe(babel())
+        .pipe(uglify())
+        .pipe(concat('main.min.js'))
+        .pipe(browserSync.reload({stream: true}))
+        .pipe(gulp.dest(paths.scripts.dest))
 }
 
 function watchFiles() {
-  gulp.watch(paths.scripts.src, scripts);
-  gulp.watch(paths.styles.src, styles);
-  gulp.watch(paths.html.src, html);
-
+    gulp.watch(paths.scripts.src, scripts);
+    gulp.watch(paths.styles.src, styles);
+    gulp.watch(paths.html.src, html);
 }
 
-function browserHost() {
-    console.log("browserHost started")
-    gulp.task('browserSync', function() {
-        browserSync.init({
-            server: {
-                baseDir: "./prod"
-            },
-            tunnel: true,
-            host: 'localhost',
-            port: 80,
-        })
-    })
-}
+const build = gulp.series(clean, html, gulp.parallel(styles, scripts));
+const watch = gulp.parallel(build, watchFiles, browserHost);
 
- 
-let build = gulp.series(browserHost, clean, html, gulp.parallel(styles, scripts));
-let watch = gulp.parallel(browserHost, build, watchFiles);
+const exportsFixes = exports;
+exportsFixes.clean = clean;
+exportsFixes.styles = styles;
+exportsFixes.scripts = scripts;
+exportsFixes.html = html;
 
-exports.clean = clean;
-exports.styles = styles;
-exports.scripts = scripts;
+exportsFixes.watch = watch;
+exportsFixes.build = build;
+exportsFixes.default = watch;
 
-exports.watch = watch;
-exports.build = build;
-exports.default = watch;
+
+//Иногда при использовании CLI скрипт не вставлен в основные файлы HTML,
+// поэтому вам следует добавить его вручную или использовать gulp.
+<!-- START: BrowserSync Reloading -->
+// <script type='text/javascript' id="__bs_script__">
+//<![CDATA[
+// document.write("<script async src='/browser-sync/browser-sync-client.js'><\/script>".replace("HOST", location.hostname));
+//]]>
+// </script>
+<!-- END: BrowserSync Reloading -->
